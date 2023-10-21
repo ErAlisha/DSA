abacce5c3acefbe060746d3247c2d8389d3683ba
ErAlisha <alishachhabra1111@gmail.com>
2023-10-22 04:07:33 +0530

    Making main.cpp to learn branching

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//Trying to learn branching,by implementing different Sorting and Searching Techniques in this branch 
+//In this branch I'll write code for int main, and for the concerned branch,I'll make matching function call for that particular branch
+
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"Learning different Searching and Sorting techniques";
+cout<<endl;
+}
