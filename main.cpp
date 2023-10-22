82d73749feff82d74fd3c0261f3551600ef441a5
ErAlisha <alishachhabra1111@gmail.com>
2023-10-22 13:33:55 +0530

    Pushing bubble sort code

--- main.cpp
+++ main.cpp
@@ -56,8 +56,20 @@ using namespace std;
 
 
 
-void bubbleSort(){
-
+vector<int>bubbleSort(vector<int>&nums){
+      int n = nums.size();
+        for(int i=0;i<n-1; i++)
+        {
+            for(int j=0; j<n-i-1; j++)
+            {
+                if(nums[j]> nums[j+1])
+                {
+                    swap(nums[j+1],nums[j]);
+                    
+                }
+            }
+        }
+        return nums;
 }
 
 
